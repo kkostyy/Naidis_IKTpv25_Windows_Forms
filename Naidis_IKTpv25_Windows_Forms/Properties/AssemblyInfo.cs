77e399556401f54f777de5030782eb89e776523f
opilane <opilane@HP-CZC2349HV2>
2026-09-11 08:49:40 +0300

    .

--- Naidis_IKTpv25_Windows_Forms/Properties/AssemblyInfo.cs
+++ Naidis_IKTpv25_Windows_Forms/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("52727fe6-b5d6-442b-b907-5490ffebfda0")]
+[assembly: Guid("05a6c1ae-759c-463f-b94c-768f79933530")]
 
 // Version information for an assembly consists of the following four values:
 //
